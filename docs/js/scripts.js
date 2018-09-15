4f2596d2134eee3737599f0b1bd2b073f1f75296
Danny Painter <dannyjpainter@gmail.com>
2018-09-15 17:39:24 -0400

    site update

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,7 +1,22 @@
-$(document).ready(function() {
+/////////////////////////////////////////////////SCROLL NAV
+$(document).ready(function(){
+  var scrollTop = 0;
+  $(window).scroll(function(){
+    scrollTop = $(window).scrollTop();
 
-  $(".menu").click(function() {
-    $(".mobile-nav").toggleClass("is-closed");
+    if (scrollTop >= 100) {
+      $('.primary-header').addClass('scrolled-nav');
+    } else if (scrollTop < 100) {
+      $('.primary-header').removeClass('scrolled-nav');
+    }
   });
-
+  
+  switch (window.location.href) {
+      case '/index':
+          $('#work-nav').addClass('selected')
+      case '/about':
+          $('#info-nav').addClass('selected')
+      case '/contact':
+          $('#contact-nav').addClass('selected')
+  };
 });
