e42d5502c9efdd311f3aabbfd794091872e1775b
Danny Painter <dannyjpainter@gmail.com>
2018-09-16 15:15:42 -0400

    updated nav sticky
    changed the nav to sticky

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,22 +1,24 @@
 /////////////////////////////////////////////////SCROLL NAV
 $(document).ready(function(){
   var scrollTop = 0;
+
   $(window).scroll(function(){
     scrollTop = $(window).scrollTop();
 
-    if (scrollTop >= 100) {
-      $('.primary-header').addClass('scrolled-nav');
-    } else if (scrollTop < 100) {
-      $('.primary-header').removeClass('scrolled-nav');
+    if (scrollTop >= 80) {
+      $('.logo').addClass('scrolled-nav');
+    } else if (scrollTop < 80) {
+      $('.logo').removeClass('scrolled-nav');
     }
   });
-  
-  switch (window.location.href) {
-      case '/index':
-          $('#work-nav').addClass('selected')
-      case '/about':
-          $('#info-nav').addClass('selected')
-      case '/contact':
-          $('#contact-nav').addClass('selected')
-  };
+
+  $(function() {
+    var pgurl = window.location.href.substr(window.location.href
+
+    .lastIndexOf("/")+1);
+         $("nav a").each(function(){
+              if($(this).attr("href") == pgurl || $(this).attr("href") == '' )
+              $(this).addClass("selected");
+         })
+    });
 });
